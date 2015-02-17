5ac3c8b9e631d3d8eb73fbcbb615f65e3f56b696
jiyinyiyong <jiyinyiyong@gmail.com>
2015-02-17 22:50:34 +0800

    basic SourceMap support

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 cirru = require('cirru-script')
+loaderUtils = require('loader-utils')
 
 module.exports = function(source) {
   this.cacheable()
+  request = loaderUtils.getRemainingRequest(this)
   options = {
-    path: 'fake-path',
-    base: 'fake-base',
-    relativePath: 'fake-relative'
+    path: request,
+    base: './',
+    relativePath: request
   }
   res = cirru.compile(source, options)
+  map = res.mapping
+  map.sourcesContent = [source]
   this.callback(null, res.js, res.mapping)
 }
